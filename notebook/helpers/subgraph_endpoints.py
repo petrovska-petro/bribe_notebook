84a0e6ffe5c108d724103c5159c3161dd8077a7e
Petrovska <petrovska@badger.finance>
2022-11-10 21:00:07 +0700

    Include next round, adding info around reth/badger bribe

--- notebook/helpers/subgraph_endpoints.py
+++ notebook/helpers/subgraph_endpoints.py
@@ -7,6 +7,7 @@ LLAMA_DASHBOARD_URL = "https://api.llama.airforce//dashboard"
 
 POOL_ID_BADGER = "0xb460daa847c45f1c4a41cb05bfb3b51c92e41b36000200000000000000000194"
 POOL_ID_DIGG = "0x8eb6c82c3081bbbd45dcac5afa631aac53478b7c000100000000000000000270"
+POOL_ID_BADGE_RETH = "0xe340ebfcaa544da8bb1ee9005f1a346d50ec422e000200000000000000000396"
 
 PROPOSAL_INFO_QUERY = """
         query($proposal_id: String) {
@@ -61,6 +62,15 @@ def get_tvl_balancer_digg_pool():
     response_tvl_badger = requests.post(
         BALANCER_SUBGRAPH,
         json={"query": TVL_QUERY, "variables": {"pool_id": POOL_ID_DIGG}},
+    ).json()
+    tvl_badger = float(response_tvl_badger["data"]["pool"]["totalLiquidity"])
+    return tvl_badger
+
+
+def get_tvl_balancer_badgerreth_pool():
+    response_tvl_badger = requests.post(
+        BALANCER_SUBGRAPH,
+        json={"query": TVL_QUERY, "variables": {"pool_id": POOL_ID_BADGE_RETH}},
     ).json()
     tvl_badger = float(response_tvl_badger["data"]["pool"]["totalLiquidity"])
     return tvl_badger
