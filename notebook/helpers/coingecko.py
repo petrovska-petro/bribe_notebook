cea4d152b3e2863299eb4c92b8802941e4b4e3f9
Petrovska <petrovska@badger.finance>
2022-11-17 17:43:56 +0700

    feat: update round 11 with rocket coincentive bribe and latest prices/pool captures

--- notebook/helpers/coingecko.py
+++ notebook/helpers/coingecko.py
@@ -2,9 +2,12 @@ from pycoingecko import CoinGeckoAPI
 
 
 def get_prices():
-    prices = CoinGeckoAPI().get_price(["aura-finance", "balancer", "badger-dao"], "usd")
+    prices = CoinGeckoAPI().get_price(
+        ["aura-finance", "balancer", "badger-dao", "rocket-pool"], "usd"
+    )
     bal_price = prices["balancer"]["usd"]
     aura_price = prices["aura-finance"]["usd"]
     badger_price = prices["badger-dao"]["usd"]
+    rpl_price = prices["rocket-pool"]["usd"]
 
-    return bal_price, aura_price, badger_price
+    return bal_price, aura_price, badger_price, rpl_price
